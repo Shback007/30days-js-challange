e3053c018492876a891023ba5a646a6e4622c32f
Sagnik Hore <sagnikhoredht@gmail.com>
2024-08-02 12:47:45 +0530

    Day20

--- Day20/index.js
+++ Day20/index.js
@@ -1,24 +1,23 @@
+//Activity 1
+console.log("\nActivity 1");
+console.log("\nTask 1");
 localStorage.setItem('myStringKey', 'Hello, World!');
 
 const retrivedValue = localStorage.getItem('myStringKey');
 
 console.log(retrivedValue);
 
-// Define an object
+console.log("\nTask 2");
 const myObject = {
     name: "John Doe",
     age: 30,
     occupation: "Developer"
   };
   
-  // Convert the object to a JSON string and save it to localStorage
   localStorage.setItem('myObject', JSON.stringify(myObject));
   
-  // Retrieve the JSON string from localStorage
   const retrievedObjectString = localStorage.getItem('myObject');
   
-  // Parse the JSON string back into an object
   const retrievedObject = JSON.parse(retrievedObjectString);
-  
-  // Log the object to the console
+
   console.log(retrievedObject);
